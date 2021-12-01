3e36af20af67650dcc70c7f65156a7530a1922f0
eddie ruxton <ae.ruxton@gmail.com>
2021-12-01 20:57:19 +0000

    backend-stage-1 complete - next step add serializers

--- photoshare/art/views.py
+++ photoshare/art/views.py
@@ -1,5 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status # to send status code
+
+from .models import Art
+
 def home(request):
     return HttpResponse('<h1>hello world</h1>')
