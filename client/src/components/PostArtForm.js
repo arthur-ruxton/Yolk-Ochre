9fa942ee14e8fcc28c8b456595b0592d5d6eeeda
eddie ruxton <ae.ruxton@gmail.com>
2021-12-09 09:37:48 +0000

    post-art works basically

---
+++ client/src/components/PostArtForm.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+
+const PostArtForm = ({ formInputProps }) => {
+  const handleFormChange = formInputProps.handleFormChange
+
+  return (
+    <>
+      <Form.Control
+        name='image'
+        type='text'
+        placeholder='Image Url'
+        onChange={handleFormChange}
+        {...formInputProps}
+      />
+      <Form.Control
+        name='caption'
+        type='text'
+        placeholder='Caption'
+        onChange={handleFormChange}
+        {...formInputProps}
+      />
+      <Form.Control
+        name='location'
+        type='text'
+        placeholder='Location'
+        onChange={handleFormChange}
+        {...formInputProps}
+      />
+    </>
+  )
+}
+
+export default PostArtForm
--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Form from 'react-bootstrap/Form'
+import PostArtForm from '../components/PostArtForm'
+import { getToken } from '../helpers/auth'
 
 const NewPost = () => {
+  const [data, setData] = useState({
+    image: '',
+    caption: '',
+    location: '',
+    owner: '1',
+  })
+
+  const [errorInfo, setErrorInfo] = useState({})
+  const [isError, setIsError] = useState(false) 
+
+  const navigate = useNavigate()
+
+  const handleError = (error) => {
+    if (error.response) {
+      setErrorInfo(error.response.data)
+      setIsError(true)
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const config = {
+      method: 'post',
+      url: '/api/art/',
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+      data,
+    }
+
+    try {
+      const { data } = await axios(config).catch(handleError)
+      console.log(data)
+      setIsError(false)
+      navigate(`/view-one-post/${data.id}/`)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target
+    setData({
+      ...data,
+      [name]: value,
+    }),
+    console.log(data)
+  }
+
+  const formInputProps = { data, errorInfo, handleFormChange }
+
   return (
     <div>
-      <p>new post</p>
+      <section className="form-section">
+        <div className="form-box">
+          <h1>Post</h1>
+          <Form onSubmit={handleSubmit} className="form">
+            <PostArtForm formInputProps={formInputProps} />
+            <div>
+              <Form.Control id="submit-button" type="submit" value="Post" />
+            </div>
+            {isError ? (
+              <div className="error">
+                <p>Error. Please try again</p>
+              </div>
+            ) : (
+              <></>
+            )}
+          </Form>
+        </div>
+      </section>
     </div>
   )
 }
